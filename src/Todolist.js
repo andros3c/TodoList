0aa96eac2d82cb49c22fc82d621f88f7657bd653
andros3c <alehandro4354@gmail.com>
2022-06-07 14:42:35 +0700

    add input new list to local storage

--- src/Todolist.js
+++ src/Todolist.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 
 function useLocalStorage(name,initialValue){
-    const[value,setValue] = React.useState(window.localStorage.getItem(name)??initialValue)
+    const[value,setValue] = React.useState(JSON.parse(window.localStorage.getItem(name))??initialValue)
 
     const stringifiedValue= JSON.stringify(value)
     React.useEffect(()=>{
@@ -12,15 +12,38 @@ function useLocalStorage(name,initialValue){
     return[value,setValue]
 }
 
+function Todo(){
+   
+}
+
+
 
 export default function TodoList(){
 
 
+const [value,setValue]=React.useState("")
+const [local,setLocal]=useLocalStorage("todoList",{"todo":[],})
 
 
+function HandleSubmit(e){
+    e.preventDefault()
+    if (value){
+        
+        setLocal({
+            ...local,
+            todo:[...local.todo,value,],
+        })
+        setValue("")
+    }
+}
     return(
         <div>
+            <form onSubmit={HandleSubmit}>
             <h1>TodoList</h1>
+            <input value={value} onChange={(e)=>setValue(e.target.value)}></input>
+            </form>
+
+            <Todo/>
         </div>
     )
 }
